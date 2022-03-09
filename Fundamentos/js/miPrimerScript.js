0597b52fe060031817cc02284d93bdf590f8de66
DaniCis <danicisneros2@hotmail.com>
2022-03-08 20:04:58 -0500

    dom y js

---
+++ Fundamentos/js/miPrimerScript.js
@@ -0,0 +1,18 @@
+/*Condicionales */
+
+let edad = 15
+let minEdad = 16
+if(edad < minEdad)
+    console.log('No puedes ver la pelicula')
+else
+    console.log('Puedes ver la pelicula')
+
+
+let clima = 'soleado'
+if(clima =='soleado')
+    console.log('Ponte bloqueador')
+else
+    console.log('Lleva paraguas')
+
+for(let i=0; i<10; i++)
+    console.log(i)
